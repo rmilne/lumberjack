5f43d374073cd6786a022c019469a9d589d20dda
rmilne <russmilne@gmail.com>
2015-12-26 20:07:22 -0500

    - initial commit, simple log splitting.  - log rotation and truncate testing needed

---
+++ lumberjack.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+
+import json
+import logging
+import os
+import pprint
+import sys
+import time
+
+from pygtail import Pygtail
+
+
+logging.basicConfig(filename='im_a_lumberjack.log',
+        format='%(asctime)s %(levelname)s %(message)s', level=logging.DEBUG)
+#logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',
+#        level=logging.DEBUG)
+log = logging.getLogger()
+
+class Splitter():
+    def __init__(self, path, patterns):
+        self.path = path
+        self.patterns = patterns
+
+    def match(self, line):
+        for pattern in self.patterns:
+            if pattern in line:
+                log.debug('Splitter.match:"%s" ==> "%s"' % (pattern, self.path))
+                self.write(line)
+                return True
+        return False
+
+    def write(self, line):
+        if not os.path.exists(self.path):
+            try:
+                os.makedirs(os.path.dirname(self.path))
+            except OSError as e:
+                if e.errno == 17:
+                    pass
+                else:
+                    raise
+        with open(self.path, 'a') as f:
+            f.write('%s\n' % line)
+
+
+class FileFollower():
+    '''
+    Use pygtail to keep track of EOF and rotated files, catch exceptions to
+    make things more seamless
+    '''
+    def __init__(self, path):
+        self.path = path
+        self.pygtail = Pygtail(path)
+
+    def next(self):
+        line = ''
+        try:
+            line = self.pygtail.next()
+        except StopIteration as si:
+            return False
+        return line
+
+
+class LumberJack():
+    def __init__(self, settings):
+        log.debug('settings: %s' % pprint.pformat(settings))
+        self.settings = settings
+        self.splitters = []
+        self.follower = FileFollower(self.settings["log_path"])
+        for split, patterns in self.settings["log_splits"].items():
+            log.debug("split: %s , patterns: %s" % (split, patterns))
+            self.splitters.append(Splitter(split, patterns))
+
+    def chop_forever(self):
+        log.debug('chop_forever')
+        while True:
+            line = self.follower.next()
+            if line == False:
+                # take a break big guy
+                log.debug('no line found, rest for %s' % self.settings["rest"])
+                time.sleep(self.settings["rest"])
+            else:
+                found_match = False
+                for chop in self.splitters:
+                    matched = chop.match(line)
+                    if matched:
+                        found_match = True
+                if not found_match:
+                    log.debug('no match: %s' % line)
+
+def usage():
+    sys.stderr.write('usage: %s [settings file]\nSettings file required if '
+                     'it does not exist in the current path\n' % sys.argv[0])
+    sys.exit(1)
+
+def main():
+    settings_path = './settings.json'
+    if not os.path.isfile(settings_path):
+        # look for settings arg
+        if not len(sys.argv) >= 2:
+            usage()
+        settings_path = sys.argv[1]
+    with open(settings_path) as sfile:
+        settings = json.load(sfile)
+    ljack = LumberJack(settings)
+    ljack.chop_forever()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception as e:
+        log.exception('Unexpected exception: %s' % e)
+        sys.exit(1)
+
+
---
+++ readme.md
@@ -0,0 +1,34 @@
+Lumberjack
+==========
+
+I'm a lumberjack, and I'm okay.
+I sleep all night and I work all day.
+
+Lumberjack follows a log file and splits it into other log files if they match
+patterns in the configuration file.
+
+Requires: pygtail  (sudo pip install pygtail)
+Does not require: women's clothing and hanging around in bars
+
+Usage
+-----
+
+python lumberjack.py settings.json
+
+
+Config
+------
+log_path: the log file to follow
+log_splits: the files to put log lines into if they match any one of the list of patterns (simple substring match)
+rest: time to rest if at the end of the log file
+
+{
+    "log_path": "test.log",
+    "log_splits": {
+        "output_example/path.log": ["pattern1", "patternN"],
+        "output_example/silly.log": ["silly walk"]
+    },
+    "rest": 5
+}
+
+
